9e094d05714940958c60a72d27d96fdb3424d858
asher <aw0534145493@gmail.com>
2025-07-19 21:59:24 +0300

    adding a new class 'classsifier'

---
+++ classifier.py
@@ -0,0 +1,8 @@
+import pandas as pd
+class Classifier:
+    def __init__(self, data_frame, target_variable):
+        self.data_frame = data_frame
+        self.target_variable = target_variable
+
+    def classifier(self):
+         

bf0a6acb244827c87cc5925bf068a56aace689aa
asher <aw0534145493@gmail.com>
2025-07-19 22:12:29 +0300

    adding filling with zeros method

--- classifier.py
+++ classifier.py
@@ -5,4 +5,17 @@ class Classifier:
         self.target_variable = target_variable
 
     def classifier(self):
-         
+
+
+    def filling_with_zeros(self):
+        dictionary_with_zeros = {}
+        for target_variable in self.data_frame[self.target_variable].unique():
+            if target_variable not in dictionary_with_zeros:
+                dictionary_with_zeros[target_variable] = {}
+            for column in self.data_frame.columns:
+                unique_values = self.data_frame[column].unique()
+                if column not in dictionary_with_zeros[target_variable]:
+                    dictionary_with_zeros[target_variable][column] = {}
+                for u in unique_values:
+                    dictionary_with_zeros[target_variable][column][u] = 0
+        return dictionary_with_zeros

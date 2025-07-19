1b0992220132634d0b3a09924d01aae615d5dccb
asher <aw0534145493@gmail.com>
2025-07-20 01:15:13 +0300

    fixing the class for csv

--- CsvCleaner.py
+++ CsvCleaner.py
@@ -1,7 +1,8 @@
 import pandas as pd
 class CsvCleanerForClassifiers:
     @staticmethod
-    def basic_data_cleaner(df):
+    def basic_data_cleaner(csv_file):
+        df = pd.read_csv(csv_file)
         df = df.drop_duplicates()
         df = df.dropna()
         for i in df.columns:

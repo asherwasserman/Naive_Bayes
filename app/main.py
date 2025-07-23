d11686720f3b13595df0198e0a4b51d598283d2b
asher <aw0534145493@gmail.com>
2025-07-23 09:49:20 +0300

    v0 ready to run

---
+++ app/main.py
@@ -0,0 +1,24 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+import pandas as pd
+from Cleaner import CsvCleanerForClassifiers
+from Trainer import Trainer
+from Classifier import Classifier
+
+app = FastAPI()
+path = r'C:\Users\user1\PycharmProjects\PythonProject3\NaiveBayesClassifier\data\data for NB buys computer - Sheet1.csv'
+df = CsvCleanerForClassifiers.basic_data_cleaner(path)
+classifier = Trainer(df, "Buy_Computer")
+probability_dictionary = classifier.probability_dictionary()
+sum_target_dict = classifier.sum_target_variable
+@app.get("/predict")
+async def predict(request: Request):
+    features = dict(request.query_params)
+    if not features:
+        return {"error": "No input features provided"}
+    prediction = Classifier(probability_dictionary, sum_target_dict).classification(features)
+    return {"prediction": prediction}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host= "127.0.0.1", port=8000)
